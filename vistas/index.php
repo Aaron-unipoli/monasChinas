a33c519aa08927499fe63262fcf81007a98788c7
Aaron-unipoli <a.martinez.isw@unipolidgo.edu.mx>
2024-12-06 17:30:16 -0600

    Corrigo direccion del la hoja de estilos se /css/estiloprincipal a ../css/estiloPrincipal

--- vistas/index.php
+++ vistas/index.php
@@ -4,7 +4,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>VsitaDgo</title>
-  <link href="css/estiloPrincipal.css" rel="stylesheet">
+  <link href="../css/estiloPrincipal.css" rel="stylesheet">
   
   <!-- Enlace a Bootstrap CSS -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
@@ -16,9 +16,9 @@
   
 
   <!-- Navbar -->
-  <nav class="navbar navbar-expand-lg navbar-dark custom-black custom-navbar custom-menu-txt fixed-top">
+  <nav class="navbar navbar-expand-lg navbar-dark custom-black custom-navbar custom-menu-txt fixed-top ">
     <div class="container-fluid">
-      <a class="navbar-brand" href="index.html"><img src="../assets/img/logo.webp" alt="logo.jpg"width="20%"> </a>
+      <a class="navbar-brand" href="index.php"><img src="../assets/img/logo.webp" alt="logo.jpg"width="20%"> </a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
